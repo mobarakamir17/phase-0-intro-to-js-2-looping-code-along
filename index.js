a906fe837003e2176c7468427f1f4b548648b1d6
Amir Mobarak <mobarakamir17@gmail.com>
2023-05-17 18:40:36 -0400

    intro to loops test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      messagesArray.push(message);
+    }
+    return messagesArray;
+  }
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
